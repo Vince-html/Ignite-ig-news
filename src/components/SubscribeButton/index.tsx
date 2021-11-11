5bf096c8e1234167ebdae5b7163ab573e48f0c9a
Vince-html <82712898+Vince-html@users.noreply.github.com>
2021-11-11 18:43:42 -0300

    Test ActiveLink n Header

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,47 @@
+import { AxiosResponse } from 'axios';
+import { signIn, useSession } from 'next-auth/client'
+import { useRouter } from 'next/router';
+import React from 'react'
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+import styles from './styles.module.scss'
+
+
+export function SubscribeButton() {
+  const [session] = useSession();
+  const router = useRouter();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return;
+    }
+    if (session.activeSubscription) {
+      router.push('/posts')
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+      const { sessionId } = response.data;
+
+
+
+      const stripe = await getStripeJs()
+
+      stripe.redirectToCheckout({ sessionId })
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={() => handleSubscribe()}
+    >
+      Subscribe Now
+    </button >
+  )
+}
